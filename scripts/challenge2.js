6ecaf63645843d87c4094b824da81d661007771a
Sarafina <sarafinajozefzoon@hotmail.com>
2018-03-18 23:29:06 +0100

    Klok opmaak 3 schermen

---
+++ scripts/challenge2.js
@@ -0,0 +1,16 @@
+var time = new Date();
+// console.log(time.getHours());
+// console.log(time.getMinutes());
+// console.log(time.getSeconds());
+
+// function setDate(){
+// 	const now = new Date();
+// 	const seconds = now.getSeconds();
+// 	console.log('seconds');
+// }
+
+// setInterval(setDate, 1000);
+
+
+// var minuutCirkel = document.getElementById('minuut');
+// var uurCirkel = document.getElementById('uur');
